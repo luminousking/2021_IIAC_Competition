a060944eccaeb58257cc4493379a1cbc81d86127
Luminousking <wxy18362037636@outlook.com>
2021-08-04 13:52:20 +0800

    Update draw2.py

--- Data_Processing/draw2.py
+++ Data_Processing/draw2.py
@@ -11,3 +11,18 @@ import matplotlib.pyplot as plt
 
 # data processing
 from sklearn import preprocessing
+
+
+df = pd.read_excel('tsdata.xlsx')
+grouped = df.groupby('ID')
+for i in range(210625,210849):
+    dfx = grouped.get_group(i) #.to_csv('test1.csv',encoding='utf_8_sig'
+    break
+
+
+
+
+
+def normalization(dfx):
+    range = np.max(dfx['']) - np.min(dfx[''])
+    return (dfx[''] - np.min(dfx[''])) / range
